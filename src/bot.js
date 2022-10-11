eba07d9fbb65be99bd409b0ed7b665e5987781e3
Jeongwook Oh <hiphophammer@hotmail.com>
2022-10-10 23:31:03 -0700

    completed basic config

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
-    console.log('Logged in!')
-})
+    console.log(`Logged in as ${client.user.tag}!`);
+});
 
 client.login(process.env.TOKEN);
