d8b55f717c65b0f333745914a4fb655f92bbf99d
Jeongwook Oh <hiphophammer@hotmail.com>
2022-10-13 05:23:19 -0700

    Added Discord.GatewayIntentBits.MessageContent
    Discord.GatewayIntentBits.MessageContent is needed to access an Discord.Message object

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 
 const Discord = require('discord.js');
-const client = new Discord.Client({ intents: [Discord.GatewayIntentBits.Guilds, Discord.GatewayIntentBits.GuildMessages] });
+const client = new Discord.Client({ intents: [
+    Discord.GatewayIntentBits.Guilds, 
+    Discord.GatewayIntentBits.GuildMessages,
+    Discord.GatewayIntentBits.MessageContent
+]});
 const fs = require('node:fs');
 const path = require('node:path')
 
