5ffeb4046e095cceaaaddd59437297576ef8ec03
Jeongwook Oh <hiphophammer@hotmail.com>
2022-10-13 03:56:51 -0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const { Client, GatewayIntentBits } = require('discord.js');
+const Discord = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
 const fs = require('fs');
 
@@ -9,6 +10,16 @@ const valid_msg = {
 };
 
 client.commands = new Discord.Collection()
+
+client.commands.load = dir => {
+    for (const file of fs.readdirSync(dir)) {
+        const cmd = require(`${dir}/${file}`);
+        client.commands.set(cmd.name, cmd);
+    }
+    console.log('The command ' + client.commands.map(c => c.name).join(', ') + ' has been loaded.');
+}
+
+client.commands.load(__dirname + "/commands");
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
