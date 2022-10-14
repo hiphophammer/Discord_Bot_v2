fcb4de7c8f7597b5b272436afd58e2eba6a2b90f
Jeongwook Oh <hiphophammer@hotmail.com>
2022-10-14 01:57:02 -0700

    Added postgreSQL connection

--- src/bot.js
+++ src/bot.js
@@ -1,17 +1,33 @@
 require('dotenv').config();
 
+const fs = require('node:fs');
+const path = require('node:path')
+
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: [
     Discord.GatewayIntentBits.Guilds, 
     Discord.GatewayIntentBits.GuildMessages,
     Discord.GatewayIntentBits.MessageContent
 ]});
-const fs = require('node:fs');
-const path = require('node:path')
 
-const valid_msg = {
+const { Client } = require('pg');
 
-};
+const client_sql = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client_sql.connect();
+
+client_sql.query( 'SELECT table_schema,table_name FROM information_schema.tables', (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+        console.log(JSON.stringify(row));
+    }
+    client_sql.end;
+});
 
 client.commands = new Discord.Collection()
 const commands_path = path.join(__dirname, 'commands');
@@ -31,12 +47,18 @@ client.on('ready', () => {
 
 client.on('messageCreate', msg => {
     if (msg.author.bot) return; // don't react to messages from bots
-    console.log("Received: " + msg.content + "!");
+    console.log("Received: " + msg.content);
 });
+
+// error handling
 
 client.on('shardError', error => {
 	console.error('A websocket connection encountered an error:', error);
 });
 
+process.on('unhandledRejection', (reason, promise) => {
+    console.log('Unhandled rejection at ', promise, `reason: ${err.message}`);
+    process.exit(1);
+});
 
 client.login(process.env.MY_TOKEN);
