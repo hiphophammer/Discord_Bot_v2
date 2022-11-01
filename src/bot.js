47c1dcf8bc450a72c7a19805514b55e6f0b2ffa3
Jeongwook Oh <hiphophammer@hotmail.com>
2022-10-31 23:18:40 -0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv';
-import { Client as DiscordClient, Events, GatewayIntentBits } from  'discord.js';
+import { Client as DiscordClient, Collection, Events, GatewayIntentBits } from  'discord.js';
+import * as fs from 'fs';
 dotenv.config();
 
 const discordClient = new DiscordClient( { intents: 
@@ -9,10 +10,16 @@ const discordClient = new DiscordClient( { intents:
         GatewayIntentBits.MessageContent
     ]});
 
+discordClient.commands = new Collection();
+
+discordClient.commands.load = dir => {
+    for ( const file of fs.readdirSync( dir ) ) {
+        
+    }
+};
+
 discordClient.once( Events.ClientReady, c => {
     console.log(`Logged in as ${ c.user.tag }!`);
 });
-
-
 
 discordClient.login( process.env.DISCORD_TOKEN );
